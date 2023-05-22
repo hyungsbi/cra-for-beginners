0f711856904ffbd53be0f43dbb81fd2c952be437
hyungsbi <hyungsbi@gmail.com>
2023-05-22 23:40:42 +0900

    [UPDATE] Completed #7.1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { useState } from "react";
+
 function App() {
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  }
+  console.log(toDos);
+  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
   return (
     <div>
-      <input type="text" placeholder="Write your todo..." />
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your todo..." />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
     </div>
   );
 }
