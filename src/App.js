223f0393c3a3cbd59449f55edd1fc7026cd64aed
hyungsbi <hyungsbi@gmail.com>
2023-05-24 22:35:21 +0900

    [UPDATE] Completed #7.2 Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [amount, setAmount] = useState("0");
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then(response => response.json())
+      .then(json => {
+
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  const findCoin = id => {
+    return coins.find(coin => coin.id === id);
   }
-  console.log(toDos);
-  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
+  const onMoneyChange = (event) => {
+    setMoney(event.target.value);
+    const select = document.getElementById('coinList');
+    console.log(select.options[select.selectedIndex].id);
+    console.log(select.options[select.selectedIndex].value);
+    const id = select.options[select.selectedIndex].id;
+    const coin = findCoin(id);
+    setAmount(parseInt(event.target.value) / parseInt(coin.quotes.USD.price));
+  }
+  const onSelectChange = (event) => {
+    const select = event.target;
+    console.log(select.options[select.selectedIndex].id);
+    console.log(select.options[select.selectedIndex].value);
+    const id = select.options[select.selectedIndex].id;
+    const coin = findCoin(id);
+    setAmount(parseInt(money) / parseInt(coin.quotes.USD.price));
+  }
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder="Write your todo..." />
-        <button>Add To Do</button>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <select id="coinList" onChange={onSelectChange}>
+        {coins.map((coin) => (
+          <option key={coin.id} id={coin.id}>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+          </option>
+        ))}
+      </select>
+      <form>
+        <div><label htmlFor="money">Money(USD) $</label><input onChange={onMoneyChange} id="money" type="number" placeholder="Money to buy coin" value={money} /></div>
+        <div><label htmlFor="amount">Coins could be bought</label><input id="amount" type="text" value={amount} readOnly /></div>
       </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => <li key={index}>{item}</li>)}
-      </ul>
     </div>
   );
 }
